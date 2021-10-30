f84a6f9c2ea62ba91782c946757955098bd04113
Paul Price <price@astro.princeton.edu>
2021-10-30 18:21:52 +0100

    blinky: add lifetime option
    so it doesn't run forever.

--- bin/blinky.py
+++ bin/blinky.py
@@ -6,13 +6,20 @@ import argparse
 
 from light_show import LedController, StripType, ColorHSV
 
-def blinky(num, gpio, pause=1, type=StripType.GRB):
+def blinky(num, gpio, pause=1, lifetime=0, type=StripType.GRB):
     controller = LedController(gpio, num, StripType.GRB)
     strip = controller[0]
+
+    runtime = 0
     while True:
         strip.fill(ColorHSV(random.random(), 1.0, 1.0).toRGB())
         controller.render()
         time.sleep(pause)
+        runtime += pause
+        if lifetime > 0 and runtime > lifetime:
+            break
+    strip.clear()
+    controller.render()
 
 
 def main():
@@ -21,8 +28,9 @@ def main():
     parser.add_argument("--gpio", type=int, default=12, help="GPIO pin (not physical pin)")
     parser.add_argument("--pause", type=float, default=1.0, help="Pause between blinks (sec)")
     parser.add_argument("--type", default="GRB", help="Type of LED strip")
+    parser.add_argument("--lifetime", type=float, default=0, help="Lifetime of blinking")
     args = parser.parse_args()
-    blinky(args.num, args.gpio, args.pause, getattr(StripType, args.type))
+    blinky(args.num, args.gpio, args.pause, args.lifetime, getattr(StripType, args.type))
 
 
 if __name__ == "__main__":
